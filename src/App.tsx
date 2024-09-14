89b1105683a209a42543ce59530428c79b56dea1
netlogs <2406292852@qq.com>
2024-09-14 16:51:40 +0800

    add piliang

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Wukong Token领取查询</h1>
+        <h1>Wukong Token批量领取查询</h1>
         <AddressChecker />
       </header>
       <footer className="App-footer">
